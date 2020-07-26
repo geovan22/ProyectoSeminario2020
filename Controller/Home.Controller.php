0ded6a4af699f3f6c7d5405293489b97dbc5c778
geovan22 <geovan221315@hotmail.com>
2020-07-25 23:13:37 -0600

    update
    cambios

--- Controller/Home.Controller.php
+++ Controller/Home.Controller.php
@@ -7,6 +7,13 @@
             $smarty->assign('title','Colegio');
             $smarty->display('Default.tpl');
         }
+
+        public function funcion()
+        {
+            $smarty = new Smarty();
+            //$smarty->assign('title','Colegio');
+            $smarty->display('p.tpl');
+        }
     }
     
 ?>
